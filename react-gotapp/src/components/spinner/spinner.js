0da1beb69e915ffdc6d668f8a4e0bef0fbed687a
Khristodulov <pkhristodulov12@ukr.net>
2020-12-10 20:28:46 +0200

    Finished the second task

---
+++ react-gotapp/src/components/spinner/spinner.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './spinner.css';
+
+const Spinner = () => {
+    return (
+        <div className="loadingio-spinner-spinner-jn6ykjitesn">
+            <div className="ldio-pdgpl91yfgp">
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
+        </div>
+    )
+}
+
+export default Spinner;
