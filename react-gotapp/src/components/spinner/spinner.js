9151ab30954153f9d1aa46e7028ab373b57c401a
Khristodulov <pkhristodulov12@ukr.net>
2020-12-17 21:14:56 +0200

    Add toggle button

--- react-gotapp/src/components/spinner/spinner.js
+++ react-gotapp/src/components/spinner/spinner.js
@@ -3,21 +3,8 @@ import './spinner.css';
 
 const Spinner = () => {
     return (
-        <div className="loadingio-spinner-spinner-jn6ykjitesn">
-            <div className="ldio-pdgpl91yfgp">
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-            </div>
+        <div className="lds-css ng-scope">
+            <div className="lds-spin"><div><div></div></div><div><div></div></div><div><div></div></div><div><div></div></div><div><div></div></div><div><div></div></div><div><div></div></div><div><div></div></div></div>
         </div>
     )
 }
